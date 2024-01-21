33ffcca6d4085d4a90f8576db074320f43437f1f
ZsirosAdam <tassigny4@gmail.com>
2024-01-21 11:12:05 +0100

    Revert "PartLogic"
    This reverts commit a26353a59eab5eda88661783981787eb9a144aa0.

--- GF5BAB_SOF_2023241_Webapp/Controllers/PartController.cs
+++ GF5BAB_SOF_2023241_Webapp/Controllers/PartController.cs
@@ -1,5 +1,4 @@
 ﻿using GF5BAB_SOF_2023241_Webapp.Data;
-using GF5BAB_SOF_2023241_Webapp.Logic;
 using GF5BAB_SOF_2023241_Webapp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -9,23 +8,27 @@ namespace GF5BAB_SOF_2023241_Webapp.Controllers
 {
     public class PartController : Controller
     {
-        private readonly PartLogic _partLogic;
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<SiteUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public PartController(PartLogic partLogic)
+        public PartController(ApplicationDbContext db, UserManager<SiteUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            _partLogic = partLogic;
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         [Authorize]
         public IActionResult Index()
         {
-            return View("../Part/ListParts", _partLogic.GetPartsList());
+            return View("../Part/ListParts", _db.Parts);
         }
 
         [Authorize(Roles = "Engineer,Teamprincipal,Admin")]
         public IActionResult ListParts()
         {
-            return View(_partLogic.GetPartsList());
+            return View(_db.Parts);
         }
 
         [Authorize(Roles = "Engineer,Admin")]
@@ -38,9 +41,12 @@ namespace GF5BAB_SOF_2023241_Webapp.Controllers
         [Authorize(Roles = "Engineer,Admin")]
         public async Task<IActionResult> AddPart(Part part)
         {
-            if (!_partLogic.PartExists(part))
+            part.EngineerId = _userManager.GetUserId(this.User);
+            var old = _db.Parts.FirstOrDefault(t => t.SerialNumber == part.SerialNumber && t.EngineerId == part.EngineerId);
+            if (old == null)
             {
-                _partLogic.AddPart(part, this);
+                _db.Parts.Add(part);
+                _db.SaveChanges();
                 TempData["SuccessMessage"] = "Item created successfully!";
                 return RedirectToAction(nameof(ListParts));
             }
@@ -54,9 +60,11 @@ namespace GF5BAB_SOF_2023241_Webapp.Controllers
         [Authorize(Roles = "Engineer,Admin")]
         public IActionResult DeletePart(string uid)
         {
-            if (_partLogic.PartExistsUid(uid))
+            var item = _db.Parts.FirstOrDefault(t => t.Uid == uid);
+            if (item != null && item.EngineerId == _userManager.GetUserId(this.User))
             {
-                _partLogic.DeletePart(uid);
+                _db.Parts.Remove(item);
+                _db.SaveChanges();
             }
             TempData["DeleteSuccessMessage"] = "Item deleted successfully!";
             return RedirectToAction(nameof(ListParts));
