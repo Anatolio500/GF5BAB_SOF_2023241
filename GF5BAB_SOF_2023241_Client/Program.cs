12d364a22356a7095f24d9bdbf6712bce48326c4
prokkaly <prokkaly@stud.uni-obuda.hu>
2024-01-24 00:50:30 +0100

    ConsoleApp Client added, displaying aggregated data

---
+++ GF5BAB_SOF_2023241_Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using GF5BAB_SOF_2023241_Webapp.Models;
+using Newtonsoft.Json;
+using System.Net;
+
+System.Threading.Thread.Sleep(10000);
+
+var wc = new WebClient();
+var data = JsonConvert.DeserializeObject<IEnumerable<Part>>(wc.DownloadString("https://localhost:7142/api"));
+
+Console.WriteLine("Parts count in the database: " +  data.Count());
+Console.WriteLine("////////////////////////////////////////////////////////////");
+Console.WriteLine();
+
+foreach (var item in data)
+{
+
+    Console.WriteLine("Part's name: " + item.Name);
+    Console.WriteLine("Part's serial number: " + item.SerialNumber);
+    Console.WriteLine("---------------------------------------");
+}
